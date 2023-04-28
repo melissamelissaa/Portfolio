39665b9836f7c768a823e3ed7e23499debfb0644
melissamelissaa <ninichiaberashvili2002@gmail.com>
2023-04-28 23:59:28 +0400

    Add buttons and icons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
-import './App.css';
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import {
+  faTwitter,
+  faGithub,
+  faLinkedin,
+  faInstagram,
+  faFacebook,
+  faTelegram
+} from "@fortawesome/free-brands-svg-icons";
+
+const IconName = (props) => {
+  return (
+    <div>
+      <p>props.id</p>
+    </div>
+  );
+};
 function App() {
   return (
     <div className="App">
+      <h1 className="heading">Hello, I'm Nini, Junior Front-end Engineer.</h1>
+      <p className="aboutMe"></p>
+      <button>Resume</button>
+      <button>
+        <FontAwesomeIcon icon={faLinkedin} className="icon" />
+        LinkedIn
+      </button>
+      <button>
+        <FontAwesomeIcon icon={faGithub} className="icon" />
+        GitHub
+      </button>
+      <div>
+        <a href="">
+          <FontAwesomeIcon icon={faFacebook} className="icon" />
+        </a>
+        <a href="">
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+        </a>
+        <a href="">
+          <FontAwesomeIcon icon={faTelegram} className="icon" />
+        </a>
+        <a href="">
+          <FontAwesomeIcon icon={faInstagram} className="icon" />
+        </a>
+        <a href="">
+          <FontAwesomeIcon icon={faLinkedin} className="icon" />
+        </a>
+        <a href="">
+          <FontAwesomeIcon icon={faGithub} className="icon" />
+        </a>
+      </div>
     </div>
   );
 }
